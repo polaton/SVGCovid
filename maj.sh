1b82ddb13e9971f3f82b169173f5e58bcd23244c
Antoine Chemardin <antoine.chemardin@gmail.com>
2021-03-01 15:52:15 +0100

    test

--- maj.sh
+++ maj.sh
@@ -2,12 +2,10 @@
 rm NCas*
 rm RFast*
 rm RStandard*
-rm Confirmes*
-rm Morts*
+rm methode-mortsaccrjourpop*
+rm methode-confirmesaccrjourpop*
 wget http://barthes.enssib.fr/coronavirus/cartes/Rmonde/NCas
 wget http://barthes.enssib.fr/coronavirus/cartes/Rmonde/RFast
 wget http://barthes.enssib.fr/coronavirus/cartes/Rmonde/RStandard
 wget http://barthes.enssib.fr/coronavirus/cartes/Rmonde/methode-confirmesaccrjourpop
-mv methode-confirmesaccrjourpop Confirmes
 wget http://barthes.enssib.fr/coronavirus/cartes/Rmonde/methode-mortsaccrjourpop
-mv methode-mortsaccrjourpop Morts
